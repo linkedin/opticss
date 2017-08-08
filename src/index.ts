af57cc0b87e87da55be6c4f954da4e35de4bfd90
Chris Eppstein <chris@eppsteins.net>
2017-08-08 00:57:34 -0700

    Export types.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
 export {
-  OptiCSS
+  OptiCSS,
 } from "./opticss";
+export {
+  Attribute,
+  AttributeBase,
+  AttributeNS,
+  AttributeValue,
+  AttributeValueChoice,
+  Class,
+  Identifier,
+  NormalizedAttributeValue,
+  NormalizedAttributeValueChoice,
+  NormalizedTagnameValue,
+  Styleable,
+  Tagname,
+  TagnameBase,
+  TagnameNS,
+  TagnameValue,
+  TagnameValueChoice,
+  ValueAbsent,
+  ValueConstant,
+  ValueEndsWith,
+  ValueStartsAndEndsWith,
+  ValueStartsWith,
+  ValueUnknown,
+} from "./Styleable";
+export {
+  SerializedTemplateInfo,
+  Template,
+  TemplateInfo,
+  TemplateInfoFactory,
+  TemplateTypes,
+} from "./TemplateInfo";
+export {
+  default as parseSelector,
+  ParsedSelector,
+  CompoundSelector
+} from "./parseSelector";
