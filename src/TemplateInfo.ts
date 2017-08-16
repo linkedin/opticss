e4a8f8d5cc6d62d2a612d30daa4525b48575213e
Chris Eppstein <chris@eppsteins.net>
2017-08-16 13:15:51 -0700

    The template registration mechanism had some type consistency issues.

--- src/TemplateInfo.ts
+++ src/TemplateInfo.ts
@@ -36,43 +36,41 @@ export interface TemplateInfo<K extends keyof TemplateTypes> {
   serialize(): SerializedTemplateInfo<K>;
 }
 
+export type TemplateConstructors<T extends TemplateTypes = TemplateTypes> = {
+    [P in keyof T]?: (identifier: string, ..._data: any[]) => T[P];
+};
+
 /**
  * Subclasses of TemplateInfo must be registered onto the static class factory.
  * it is important for the registered name of the template info to be unique
  * from all other possible names for other types of template info.
+ *
+ * A function that accepts serialized data and returns an instance of the type must be registered so that
+ * serializable datatypes that have references to templates can transparently serialize and deserialize them.
+ *
+ * Note that when using TypeScript, the TemplateTypes interface must first have your key and template type added to it
+ * before attempting to register the deserializer function.
+ *
+ * import { TemplateTypes, TemplateInfo, SerializedTemplateInfo, TemplateInfoFactory } from "@css-blocks/opticss"
+ * declare module "@css-blocks/opticss" {
+ *   export interface TemplateTypes {
+ *     "MyTemplateTypeName": MyTemplateClass;
+ *   }
+ * }
+ *
+ * class MyTemplateClass implements TemplateInfo<"MyTemplateTypeName"> {
+ *   static deserialize(identifier: string, ...data: any[]): MyTemplateClass {
+ *     return new MyTemplateClass(identifier);
+ *   }
+ *   serialize(): SerializedTemplateInfo<"MyTemplateTypeName"> {
+ *     return { ... };
+ *   }
+ * }
+ * TemplateInfoFactory.constructors["MyTemplateTypeName"] = MyTemplateClass.deserialize;
  */
 export class TemplateInfoFactory {
-  static constructors: {
-    [K in keyof TemplateTypes]?: (identifier: string, ..._data: any[]) => TemplateTypes[K];
-  } = {};
+  static constructors: TemplateConstructors = {};
 
-  /**
-   * A function that accepts serialized data and returns an instance of the type must be registered so that
-   * serializable datatypes that have references to templates can transparently serialize and deserialize them.
-   *
-   * Note that when using TypeScript, the TemplateTypes interface must first have your key and template type added to it
-   * before attempting to register the deserializer function.
-   *
-   * import { TemplateTypes, TemplateInfo, SerializedTemplateInfo, TemplateFactory } from "@css-blocks/opticss"
-   * declare module "@css-blocks/opticss" {
-   *   export interface TemplateTypes {
-   *     "MyTemplateTypeName": MyTemplateClass;
-   *   }
-   * }
-   *
-   * class MyTemplateClass implements TemplateInfo<"MyTemplateTypeName"> {
-   *   static deserialize(identifier: string, ...data: any[]): MyTemplateClass {
-   *     return new MyTemplateClass(identifier);
-   *   }
-   *   serialize(): SerializedTemplateInfo<"MyTemplateTypeName"> {
-   *     return { ... };
-   *   }
-   * }
-   * TemplateFactory.register("MyTemplateTypeName", MyTemplateClass.deserialize);
-   */
-  static register<K extends keyof TemplateTypes>(name: K, constructor: (identifier: string, ..._data: any[]) => TemplateTypes[K]) {
-    TemplateInfoFactory.constructors[name] = constructor;
-  }
   static create<K extends keyof TemplateTypes>(name: K, identifier: string, ...data: any[]): TemplateTypes[K] {
     let constructor = TemplateInfoFactory.constructors[name];
     if (constructor) {
@@ -113,4 +111,4 @@ export class Template implements TemplateInfo<"Opticss.Template"> {
   }
 }
 
-TemplateInfoFactory.register("Opticss.Template", Template.deserialize);
+TemplateInfoFactory.constructors["Opticss.Template"] = Template.deserialize;
