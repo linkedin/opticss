5c93b7f02e5f032a5c4096b4a6e31a520a2e1c26
Chris Eppstein <chris@eppsteins.net>
2017-08-23 16:00:27 -0700

    add failing test for :not() selectors.

--- test/opticss-test.ts
+++ test/opticss-test.ts
@@ -78,4 +78,23 @@ export class OptiCSSTest {
       assert.equal(optimized, `\n`);
     });
   }
+  @test "Handles :not() in removal"() {
+    let css1 = `.a { color: red; }`;
+    let css2 = `:not(.a) .b { width: 100%; }`;
+    let template = new TestTemplate("test", clean`
+      <div class="b"></div>
+    `);
+    let analyzer = new SimpleAnalyzer(template);
+    let analysis = analyzer.analyze();
+    let optimizer = new Optimizer({
+      only: ["removeUnusedStyles"]
+    });
+    optimizer.addSource({content: css1, filename: "test1.css"});
+    optimizer.addSource({content: css2, filename: "test2.css"});
+    optimizer.addAnalysis(analysis);
+    return optimizer.optimize("optimized.css").then(result => {
+      let optimized = result.output.content.toString();
+      assert.equal(optimized, `\n${css2}`);
+    });
+  }
 }
