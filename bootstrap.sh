672c594cf096fde65b323854ea57b8f930028d52
Adam Miller <ammiller@linkedin.com>
2017-10-02 11:53:13 -0700

    Fixes to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 cd packages
 if [ -d css-property-parser ]
 then
-  cd -
-  cd packages/css-property-parser
-  git pull
+  echo -e "${GREEN}✔ You already have css-property-parser checked out${NC}"
 else
   git clone git@github.com:css-blocks/css-property-parser.git
+  cd css-property-parser
+  npm install
+  npm run start
+  cd ..
 fi
-cd -
+cd ..
 
 yarn install
 yarn run bootstrap
 yarn link css-select
 yarn link postcss
+yarn link css-size
 
 for PACKAGE in resolve-cascade util template-api simple-template opticss demo-cli
 do
