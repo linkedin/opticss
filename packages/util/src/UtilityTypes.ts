46fcdeb0a5515f76ff387c4f3a11eb4043ad1881
Chris Eppstein <chris@eppsteins.net>
2017-11-04 11:12:26 -0700

    utility function for keys on an object dictionary.

--- packages/util/src/UtilityTypes.ts
+++ packages/util/src/UtilityTypes.ts
@@ -2,6 +2,11 @@ export interface ObjectDictionary<T> {
   [prop: string]: T;
 }
 
+export function objectValues<T>(dict: ObjectDictionary<T>): Array<T> {
+  let keys = Object.keys(dict);
+  return keys.map(k => dict[k]);
+}
+
 export type StringDict = ObjectDictionary<string>;
 
 export type something = string | number | boolean | symbol | object;
