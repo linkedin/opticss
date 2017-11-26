a08f4f038f130d471223fe633796af54b6710a67
Chris Eppstein <chris@eppsteins.net>
2017-11-25 20:20:33 -0800

    chore(element-analysis): Add some tests to element-analysis

--- packages/element-analysis/test/element-analysis-test.ts
+++ packages/element-analysis/test/element-analysis-test.ts
@@ -1,7 +1,11 @@
 import { assert } from 'chai';
 import { suite, test } from 'mocha-typescript';
 import {
-  attrValues
+  attrValues,
+  AttributeValueSet,
+  Element,
+  Attribute,
+  Tagname,
 } from "../src";
 
 @suite("Element Analysis")
@@ -11,5 +15,10 @@ export class ElementAnalysisTest {
     assert.deepEqual(attrValues.allOf([attrValues.constant("foo")]), {
       allOf: [{constant: "foo"}]
     });
+  }
+  @test "css-blocks class analysis"() {
+    let value: AttributeValueSet = JSON.parse('{"allOf":[{"constant":"header__emphasis"},{"constant":"typography__underline"},{"oneOf":[{"allOf":[{"constant":"with-dynamic-classes__world"},{"oneOf":[{"absent":true},{"constant":"with-dynamic-classes__world--thick"}]}]},{"absent":true}]},{"oneOf":[{"absent":true},{"constant":"header__emphasis--style-bold"},{"constant":"header__emphasis--style-italic"}]}]}');
+    let element = new Element(new Tagname(attrValues.constant('span')), [new Attribute("class", value)]);
+    assert.deepEqual(element.toString(), '<span class="header__emphasis typography__underline (with-dynamic-classes__world (---|with-dynamic-classes__world--thick)|---) (---|header__emphasis--style-bold|header__emphasis--style-italic)">');
   }
 }
