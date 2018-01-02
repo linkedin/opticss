174ed09c3bda83cec4d1762a70db48b90d7b1bdb
Chris Eppstein <chris@eppsteins.net>
2018-01-02 13:49:17 -0800

    feature(util): Make the Maybe type more full featured.

--- packages/util/src/Maybe.ts
+++ packages/util/src/Maybe.ts
@@ -1,42 +1,221 @@
+/**
+ * Maybe.ts - A TypeScript implementation of the Maybe Monad.
+ * ==========================================================
+ *
+ * Usually a Maybe is strong type with methods, but that approach, I felt,
+ * would lead to poor performance characteristics and also not take full
+ * advantage of TypeScript's specific approach to types using type guards.
+ *
+ * Other Maybe libraries found in my research, attempt to emulate pattern
+ * matching. This approach was explicitly rejected because this would incur the
+ * cost of creating new objects and functional closures unnecessarily when
+ * simple branching via type guards will suffice with only minimal impact to
+ * code legibility.
+ *
+ * In this Maybe implementation, the None has an associated error object or
+ * error message that can be provided at the point where the None was first
+ * created. If provided, that is the error which is raised when a None is
+ * unwrapped.
+ *
+ * The error message is particularly useful in combination with `attempt`,
+ * which executes a callback and if it raises an error, the execution returns a
+ * None, with the error value set accordingly.
+ *
+ * The callMaybe function can be used to conditionally skip execution of a
+ * function if any of the arguments are a None. Normal values and Maybe values
+ * can be intermixed -- any Some values are unwrapped before being passed.
+ *
+ * ## Basic Usage:
+ *
+ * ```ts
+ * import {
+ *  some, none, callMaybe, isMaybe, isSome, OptionalMaybe, Maybe,
+ * } from '@opticss/util';
+ * const LARGENESS_THRESHOLD = 100;
+ * function getLargeNumber(n: number): Maybe<number> {
+ *   if (number > LARGENESS_THRESHOLD) {
+ *     return some(number);
+ *   } else {
+ *     return none(`number must be greater than ${LARGENESS_THRESHOLD}`);
+ *   }
+ * }
+ *
+ * function formatIfLargeNumber(n: number): Maybe<string> {
+ *   let largeN = getLargeNumber(n);
+ *   return callMaybe(formatNumber, largeN);
+ * }
+ *
+ * let counter: number = 0;
+ * function updateCounter(n: OptionalMaybe<number>) {
+ *   if (isMaybe(n)) {
+ *     if (isSome(n)) {
+ *       counter += unwrap(n);
+ *     }
+ *   } else {
+ *     number += n;
+ *   }
+ * }
+ *
+ * function formatNumber(n: number): string {
+ *   return n.toString(16);
+ * }
+ * ```
+ */
 export type something = string | number | boolean | symbol | object;
+export type defined = something | null;
 
-export type Maybe<T extends something> = Some<T> | None;
-export type None = { v: undefined, error?: string };
-export type Some<T extends something> = { v: T };
+// export const MAYBE = Symbol("Maybe");
+export const NO_VALUE = Symbol("None");
+export type Maybe<T> = Some<T> | None;
+export type None = { maybe: symbol, error?: string | Error };
+export type Some<T> = { maybe: T };
+export type MaybeUndefined<T extends defined> = T | Maybe<T> | undefined;
+export type OptionalMaybe<T> = T | Maybe<T>;
 
-export function maybe<T extends something>(v: T | undefined, error?: string): Maybe<T> {
-  if (typeof v === "undefined") {
-    let n: None = {v: undefined};
-    if (error) {
-      n.error = error;
-    }
-    return n;
+/**
+ * Passes a Maybe through. If the value is not a Maybe, undefined is converted
+ * to None, all other values are treated as Some. An error message can be
+ * provided for the eventual call to none() if the value is undefined.
+ */
+export function maybe<T extends something>(v: MaybeUndefined<T>, error?: string): Maybe<T> {
+  if (v === undefined || v === null) {
+    return none(error);
+  } else if (isMaybe(v)) {
+    return v;
   } else {
-    return {v};
+    return some(v);
   }
 }
 
-export function isSome<T extends something>(value: Maybe<T>): value is Some<T> {
-  return typeof value.v !== "undefined";
+/**
+ * Creates a Some() value.
+ */
+export function some<T>(v: T): Some<T> {
+  return {maybe: v};
 }
 
-export function isNone<T extends something>(value: Maybe<T>): value is None {
-  return typeof value.v === "undefined";
+/**
+ * Creates a None() value.
+ */
+export function none(error?: string | Error): None {
+  return {maybe: NO_VALUE, error};
 }
 
+export type FunctionCall1<A1, R> = (arg1: A1) => R;
+export type FunctionCall2<A1, A2, R> = (arg1: A1, arg2: A2) => R;
+export type FunctionCall3<A1, A2, A3, R> = (arg1: A1, arg2: A2, arg3: A3) => R;
+export type FunctionCall4<A1, A2, A3, A4, R> = (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => R;
+export type CallMeMaybe1<A1, R> = FunctionCall1<A1, R>
+                                | FunctionCall1<A1, Maybe<R>>;
+export type CallMeMaybe2<A1, A2, R> = FunctionCall2<A1, A2, R>
+                                    | FunctionCall2<A1, A2, Maybe<R>>;
+export type CallMeMaybe3<A1, A2, A3, R> = FunctionCall3<A1, A2, A3, R>
+                                        | FunctionCall3<A1, A2, A3, Maybe<R>>;
+export type CallMeMaybe4<A1, A2, A3, A4, R> = FunctionCall4<A1, A2, A3, A4, R>
+                                            | FunctionCall4<A1, A2, A3, A4, Maybe<R>>;
+export type CallMeMaybe<A1, A2, A3, A4, R> = CallMeMaybe1<A1, R>
+                                           | CallMeMaybe2<A1, A2, R>
+                                           | CallMeMaybe3<A1, A2, A3, R>
+                                           | CallMeMaybe4<A1, A2, A3, A4, R>;
+
+export function callMaybe<A1, R>(fn: CallMeMaybe1<A1, R>, arg1: OptionalMaybe<A1>): Maybe<R>;
+export function callMaybe<A1, A2, R>(fn: CallMeMaybe2<A1, A2, R>, arg1: OptionalMaybe<A1>, arg2: OptionalMaybe<A2>): Maybe<R>;
+export function callMaybe<A1, A2, A3, R>(fn: CallMeMaybe3<A1, A2, A3, R>, arg1: OptionalMaybe<A1>, arg2: OptionalMaybe<A2>, arg3: OptionalMaybe<A3>): Maybe<R>;
+export function callMaybe<A1, A2, A3, A4, R>(fn: CallMeMaybe4<A1, A2, A3, A4, R>, arg1: OptionalMaybe<A1>, arg2: OptionalMaybe<A2>, arg3: OptionalMaybe<A3>, arg4: OptionalMaybe<A4>): Maybe<R>;
+
+/**
+ * If any argument is a None, do not invoke the function and return the first argument that is a none instead.
+ * If the function returns a maybe, pass it through. All other return values are returned as a Some.
+ */
+export function callMaybe<A1, A2, A3, A4, R>(fn: CallMeMaybe<A1, A2, A3, A4, R>, arg1: OptionalMaybe<A1>, arg2?: OptionalMaybe<A2>, arg3?: OptionalMaybe<A3>, arg4?: OptionalMaybe<A4>): Maybe<R> {
+  if (isNone(arg1)) { return arg1; }
+  else if (isNone(arg2)) { return arg2; }
+  else if (isNone(arg3)) { return arg3; }
+  else if (isNone(arg4)) { return arg4; }
+  let rv: OptionalMaybe<R> = fn.call(null,
+    unwrapIfMaybe(arg1),
+    arg2 && unwrapIfMaybe(arg2),
+    arg3 && unwrapIfMaybe(arg3),
+    arg4 && unwrapIfMaybe(arg4)
+  );
+  if (isMaybe(rv)) return rv;
+  return some(rv);
+}
+
+/**
+ * run the callback, if it returns a maybe, pass it through.
+ */
+export function attempt<R>(fn: () => OptionalMaybe<R>): Maybe<R> {
+  try {
+    let rv = fn();
+    if (isMaybe(rv)) return rv;
+    return some(rv);
+  } catch (e) {
+    return none(e);
+  }
+}
+
+/**
+ * Type Guard. Test if the value is a Maybe (a Some or a None).
+ */
+export function isMaybe<T extends something>(value: any): value is Maybe<T> {
+  if (value === null || value === undefined) return false;
+  return value.hasOwnProperty('maybe');
+}
+
+/**
+ * Type Guard. Test if the value is a Some.
+ */
+export function isSome<T extends something>(value: any): value is Some<T> {
+  if (value === null || value === undefined) return false;
+  return value['maybe'] !== NO_VALUE && isMaybe(value);
+}
+
+/**
+ * Type Guard. Test if the value is a None.
+ */
+export function isNone(value: any): value is None {
+  if (value === null || value === undefined) return false;
+  return value['maybe'] === NO_VALUE;
+}
+
+/**
+ * An error class that is raised when the error provided is just a string
+ * message.
+ */
 export class UndefinedValue extends Error {
-  constructor(message = "A value was expected.") {
-    super(message);
+  constructor(message?: string) {
+    super(message || "A value was expected.");
   }
 }
 
-export function unwrap<T extends something>(value: Maybe<T>): T {
+/**
+ * If the Maybe is a None, raise an error.
+ * otherwise, return the value of the Maybe.
+ */
+export function unwrap<T>(value: Maybe<T>): T {
   if (isNone(value)) {
     if (value.error) {
-      throw new UndefinedValue(value.error);
+      if (value.error instanceof Error) {
+        throw value.error;
+      } else {
+        throw new UndefinedValue(value.error);
+      }
     } else {
       throw new UndefinedValue();
     }
+  } else {
+    return value['maybe'];
   }
-  return value.v;
+}
+
+/**
+ * If the value passed is a maybe, unwrap it. otherwise pass the value through.
+ */
+export function unwrapIfMaybe<T>(value: OptionalMaybe<T>): T {
+  if (isMaybe(value)) {
+    return unwrap(value);
+  } else {
+    return value;
+  }
 }
---
+++ packages/util/test/maybe-test.ts
@@ -0,0 +1,157 @@
+import { assert } from 'chai';
+import { suite, test } from 'mocha-typescript';
+
+import { attempt, callMaybe, isMaybe, isNone, isSome, maybe, NO_VALUE, none, some, unwrap } from '../src/Maybe';
+
+@suite("Maybe")
+export class MaybeTest {
+  @test "can be constructed"() {
+    let some = maybe(0);
+    let someWithError = maybe(0, "my custom error message");
+    let none = maybe(undefined);
+    let noneWithError = maybe(undefined, "my custom error message");
+    assert.deepEqual(some, {maybe: 0});
+    assert.deepEqual(someWithError, {maybe: 0});
+    assert.deepEqual(none, {maybe: NO_VALUE, error: undefined});
+    assert.deepEqual(noneWithError, {maybe: NO_VALUE, error: "my custom error message"});
+  }
+  @test "can be checked if it isSome"() {
+    assert.isTrue(isSome(some(1)));
+    assert.isFalse(isSome(none()));
+    assert.isFalse(isSome(undefined));
+    assert.isFalse(isSome(null));
+    assert.isFalse(isSome("a string"));
+    assert.isFalse(isSome(true));
+    assert.isTrue(isSome(some(false)));
+  }
+  @test "can be checked if it isNone"() {
+    assert.isFalse(isNone(some(1)));
+    assert.isTrue(isNone(none()));
+    assert.isFalse(isNone(undefined));
+    assert.isFalse(isNone(null));
+    assert.isFalse(isNone("a string"));
+    assert.isFalse(isNone(true));
+  }
+  @test "can be checked if it isMaybe"() {
+    assert.isTrue(isMaybe(some(1)));
+    assert.isTrue(isMaybe(none()));
+    assert.isFalse(isMaybe(undefined));
+    assert.isFalse(isMaybe(null));
+    assert.isFalse(isMaybe("a string"));
+    assert.isFalse(isMaybe(true));
+  }
+  @test "unwrapping Some returns the value."() {
+    assert.equal(5, unwrap(maybe(5)));
+    assert.equal(false, unwrap(maybe(false)));
+    let s = Symbol();
+    assert.equal(s, unwrap(some(s)));
+  }
+  @test "unwrapping None throws an error."() {
+    try {
+      unwrap(none());
+      assert(false);
+    } catch (e) {
+      assert.equal(e.message, "A value was expected.");
+    }
+    try {
+      unwrap(none("foo"));
+      assert(false);
+    } catch (e) {
+      assert.equal(e.message, "foo");
+    }
+  }
+  @test "conditionally call a function with arity of 1"() {
+    let wasCalled = false;
+    let addOne = (n: number) => { wasCalled = true; return n + 1; };
+    assert.equal(unwrap(callMaybe(addOne, 4)), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(addOne, some(4))), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(addOne, none())));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+  }
+  @test "conditionally call a function with arity of 1 that accepts undefined"() {
+    let wasCalled = false;
+    let addOne = (n: number | undefined) => { wasCalled = true; return (n || 0) + 1; };
+    assert.equal(unwrap(callMaybe(addOne, 4)), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(addOne, undefined)), 1);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(addOne, some(4))), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+  }
+  @test "conditionally call a function with arity of 2"() {
+    let wasCalled = false;
+    let addTogether = (a: number, b: number) => { wasCalled = true; return a + b; };
+    assert.equal(unwrap(callMaybe(addTogether, 4, 1)), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(addTogether, some(4), 1)), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(addTogether, 4, some(1))), 5);
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(addTogether, none(), 1)));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(addTogether, 4, none())));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+  }
+  @test "conditionally call a function with arity of 3"() {
+    let wasCalled = false;
+    let makeMessage = (a: string, b: number, c: {a: number}) => { wasCalled = true; return `${a} ${b} ${c.a}`; };
+    assert.equal(unwrap(callMaybe(makeMessage, 'hi', 1, {a: 42})), "hi 1 42");
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(makeMessage, some('hi'), some(1), some({a: 42}))), "hi 1 42");
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(makeMessage, some('hi'), 1, some({a: 42}))), "hi 1 42");
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(makeMessage, none(), 1, {a: 1})));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(makeMessage, some('yo'), none(), {a: 42})));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(makeMessage, some('yo'), 5, none())));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+  }
+  @test "conditionally call a function with arity of 4"() {
+    let wasCalled = false;
+    let makeMessage = (a: string, b: number, c: {a: number}, d: boolean) => { wasCalled = true; return `${a} ${b} ${c.a} ${d}`; };
+    assert.equal(unwrap(callMaybe(makeMessage, 'hi', 1, {a: 42}, true)), "hi 1 42 true");
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.equal(unwrap(callMaybe(makeMessage, some('hi'), some(1), some({a: 42}), some(false))), "hi 1 42 false");
+    assert.isTrue(wasCalled);
+    wasCalled = false;
+    assert.isTrue(isNone(callMaybe(makeMessage, none(), 1, {a: 1}, false)));
+    assert.isFalse(wasCalled);
+    wasCalled = false;
+  }
+  @test "attempt an operation"() {
+    assert.equal(unwrap(attempt(() => 42)), 42);
+    let error = new Error("gonna throw this");
+    let maybe = attempt(() => {
+      throw error;
+    });
+    assert.isTrue(isNone(maybe));
+    try {
+      unwrap(maybe);
+      assert("should have thrown");
+    } catch (e) {
+      assert.equal(e, error);
+    }
+  }
+}
