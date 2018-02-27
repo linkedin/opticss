3f63bf2b63d967f1decd012060c0dafc3e0aa5ee
Chris Eppstein <chris@eppsteins.net>
2018-02-27 09:40:41 -0800

    feat(util): Add assertNeverCalled() helper utility.

--- packages/util/src/assertNever.ts
+++ packages/util/src/assertNever.ts
@@ -3,3 +3,7 @@ import { inspect } from "util";
 export function assertNever(value: never): never {
   throw new Error(`Unexpected value: ${inspect(value)}`);
 }
+
+export function assertNeverCalled(): never {
+  throw new Error(`Unexpected invocation.`);
+}
