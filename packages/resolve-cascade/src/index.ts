7cd49d31746f913091cf1836e061d80a55d25173
Adam Miller <ammiller@linkedin.com>
2018-10-17 08:55:49 -0700

    feat: Manually throw error for Node 6 in Optimizer.

--- packages/resolve-cascade/src/index.ts
+++ packages/resolve-cascade/src/index.ts
@@ -1,6 +1,19 @@
 /**
  * @module resolve-cascade
  **/
+
+/* tslint:disable */
+// Object.entries polyfill for Node.js 6
+// TODO: Remove April 2019 when Node.js 6 is EOL'd
+if (!(Object as any).entries) {
+  (Object as any).entries = function(obj: any){
+    let ownProps = Object.keys(obj), i = ownProps.length, resArray = new Array(i);
+    while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    return resArray;
+  };
+}
+/* tslint:enable */
+
 export * from "./Cascade";
 export * from "./util";
 export * from "./assertCascade";
